ca2e0ddcb4eb5d98b57fa6afd7888681cf162d6e
Eduard Castro <eduard.cas07@gmail.com>
2020-04-12 20:35:31 -0500

    Add vue products

---
+++ vue-products/app.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        altText: 'Socks green',
+        link: 'https://google.com'
+    }
+});
